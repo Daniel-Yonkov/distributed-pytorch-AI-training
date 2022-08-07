9f7120678a21a58050072b27e176eac94b3df622
Daniel Yonkov <daniel.yonkov@gmail.com>
2022-08-07 18:39:23 +0300

    Feat: add training based on continuous SGD

---
+++ lib/pytorch/training.py
@@ -0,0 +1,36 @@
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.distributed as dist
+from torch.autograd import Variable
+from math import ceil
+from lib.pytorch.NeuralNetwork import Net
+
+
+def trainModel(trainSet, batchSize, numberOfEpochs, rank):
+    model = Net()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    num_batches = ceil(len(trainSet.dataset) / float(batchSize))
+    for epoch in range(numberOfEpochs):
+        epoch_loss = 0.0
+        for data, target in trainSet:
+            data, target = Variable(data), Variable(target)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            epoch_loss += loss.data.item()
+            loss.backward()
+            average_gradients(model)
+            optimizer.step()
+        # Print Model Summary
+        # summary(model, input_size=(64, 1, 28, 28))
+        print('Rank ',
+              dist.get_rank(), ', epoch ', epoch, ': ',
+              epoch_loss / num_batches)
+
+
+def average_gradients(model):
+    """ Gradient averaging. """
+    size = float(dist.get_world_size())
+    for param in model.parameters():
+        dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
+        param.grad.data /= size
