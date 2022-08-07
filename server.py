7203bf4c972223a21af1b076b3b8a9076499ac13
Daniel Yonkov <daniel.yonkov@gmail.com>
2022-08-07 18:41:18 +0300

    Feat: add server integration for distributed training

---
+++ server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import time
+import socket
+from dotenv import load_dotenv
+# from torchinfo import summary
+from lib.socket import getTCPConnection,\
+    startConnectionListenerProcess, sendWorldSizeAndEpochsToPeers
+from lib.control import allPeersConnectedControlBlock
+from lib.pytorch.main import run
+
+
+load_dotenv()
+
+
+def awaitClientsConnection(socketConnection: socket, epochs) -> None:
+    connectionListenerProcess = startConnectionListenerProcess(
+        socketConnection)
+    allPeersConnectedControlBlock()
+    worldSize = sendWorldSizeAndEpochsToPeers(epochs)
+    connectionListenerProcess.terminate()
+    return worldSize
+
+
+if __name__ == "__main__":
+    epochs = 10
+    rank = 0  # server rank
+    worldSize = awaitClientsConnection(getTCPConnection(), epochs)
+    """ Initialize the distributed environment. """
+    # TODO extract into config
+    start = time.time()
+    run(rank, worldSize, epochs)
+    end = time.time()
+
+    print("Time to iterate trought", epochs, "epochs:", end - start)
